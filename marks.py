3fdf98bd64c6935eefe5077af3d537c5310e5910
sariya925 <sariyatabassum658@gmail.com>
2025-11-11 14:02:06 +0530

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,12 @@
+#marks
+mark=int(input("enter the marks"))
+if mark>=90:
+  print("excellent")
+elif mark>=70:
+  print("good")
+elif mark>=50:
+  print("average")
+elif mark==50:
+  print("just pass")
+else:
+  print("fail")
